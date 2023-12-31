a018b87d1aaeb72028802f87aa5d9a8c084868e0
Anuja Gyawali <anujagyawali000@gmail.com>
2023-12-31 07:32:59 +0545

    Assigment of Day2 completed.

--- src/ACT.jsx
+++ src/ACT.jsx
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
-const Todo = (props) => {
-    const[val, setVal] = useState('')
-    const[add, setAdd] = useState(['']);
+const Todo = () => {
+    const[val, setVal] = useState('');
+    const[add, setAdd] = useState([]);
+    const[alertMessage, setAlertMessage] = useState("");
 
     const handleFunction = (e) => {
         
@@ -11,20 +12,37 @@ const Todo = (props) => {
 
     const handleClick = (e) => {
         e.preventDefault();
-        setAdd([...add,{text: val, completed: false}]);
-        setVal('');
-    }
+        if (val.trim() !== ''){
+            setAdd([...add,{text: val, completed: false}]);
+            setVal('');
+
+           setAlertMessage("Ohh great! , You got a new task.");
+            setTimeout(() => {
+                setAlertMessage("");
+            }, 1000);
+            }
+        else {
+            setAlertMessage("Please enter a non-empty task!");
+            setTimeout(() => {
+                setAlertMessage("");
+              }, 1000);
+        }
+    };
     const handleCheckbox = (index) => {
         const updatedAdd = [...add];
         updatedAdd[index].completed = !updatedAdd[index].completed;
         setAdd(updatedAdd);
     }
+    
+
+    
 
     return (
         <div id="container">
             <form>
                 <input type="text" value={val} onChange={handleFunction}/>
-                <div><button onClick={handleClick}>Add Task</button></div>
+                <div><button onClick={handleClick}>Add Task</button>
+                   </div>
             </form>
             <div id="c">
                 {add.map((add, index)=> (
@@ -38,7 +56,11 @@ const Todo = (props) => {
                         </div>
                 ))}
             </div>
-
+            {alertMessage && (
+          <div className="alert">
+          {alertMessage}
+          </div>
+            )}
         </div>
     );
 };
